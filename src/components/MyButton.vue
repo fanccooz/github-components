63ed576919e764fa48cf7d2e2c2091279b5dde3d
ouyang <1208389898@example.com>
2025-04-01 16:22:56 +0800

    创建组件库

---
+++ src/components/MyButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { defineProps } from "vue";
+
+  defineProps<{ label: string }>();
+</script>
+
+<template>
+  <button class="my-button">{{ label }}</button>
+</template>
+
+<style>
+  .my-button {
+    padding: 10px;
+    background: blue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+  }
+</style>
